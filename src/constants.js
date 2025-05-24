df25b0b08ad22e30e26ae9ebfb6fd85111fec0e3
ArjunPathak002 <arjunpathakdev@gmail.com>
2025-05-24 15:59:39 +0530

    env setting

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,1 @@
-// export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=Your Key"
-
-export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCCAMXaYfkwJubOjq8y1GHr4PVomEAFOhA"
+export const URL=`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${import.meta.env.VITE_GEMINI_API_KEY}`
